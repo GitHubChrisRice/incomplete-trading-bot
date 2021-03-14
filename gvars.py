13a1322bde86533e9661acfb270be21d74e40ba8
GitHubChrisRice <80618407+GitHubChrisRice@users.noreply.github.com>
2021-03-14 01:02:40 -0800

    Update gvars.py

--- gvars.py
+++ gvars.py
@@ -7,7 +7,7 @@
 from pathlib import Path
 from datetime import datetime
 
-MAX_WORKERS = 10 # max threads at a time
+MAX_WORKERS = 20 # max threads at a time
 
 gainRatio = 1.6 # takeProfit = -stopLoss*gainRatio
 stopLossMargin = 0.05 # extra margin for the stop loss
@@ -16,8 +16,8 @@ operEquity = 10000 # defines the target amount per execution ($)
 limitOrderMargin = 0.1 # percentage that defines the offset for the limit orders
 
 # YOUR API KEYS AT ALPACA GO HERE!
-API_KEY = ""
-API_SECRET_KEY = ""
+API_KEY = "PKOYH6R3E0W70X9BLGLO"
+API_SECRET_KEY = "iAjwA3JcXZdwNIVHrxemY9HdhzDqWq2VwuqanUD2"
 ALPACA_API_URL = "https://paper-api.alpaca.markets"
 
 # this block checks whether you have your keys written or not
